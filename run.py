b6b16c3291e0f25b8f4aaa399895394174a19d72
Cynddl <luc.rocher@ens-lyon.fr>
2013-09-01 16:21:09 +0200

    mend

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from federation import app
-app.run(debug=True)
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
